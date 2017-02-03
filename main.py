9114388516aaf8887297466357300e15fbaf486b
Addey Welch <awelch@basecampcodingacademy.org>
2017-02-03 15:32:34 -0600

    Completed my show_inventory function to return the rental options
    Passed 1st test case

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ def show_inventory():
     Allows the user to view what inventory is in stock.
     """
     with open('inventory.txt', 'r') as file:
-        new_list = file.read().split(' - ').strip()
-    print(new_list)
+        new_list = file.read().splitlines()
+        # ['Houses - Castle Bouncer (7) - Sports Bouncer (4) - Disney Princess Bouncer (3)']
+
+        list_info = list(map(lambda s: s.split(' - '), new_list))
+        # get info in usable format
+    return list_info
+
+
+if __name__ == '__main__':
+    print(show_inventory())
