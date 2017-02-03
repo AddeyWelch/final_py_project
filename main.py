e7d9ba6518accf8d210a1237ccf3eb4076d2a20c
Addey Welch <awelch@basecampcodingacademy.org>
2017-02-03 16:17:26 -0600

    Added my functions needed for the remaining part on my program.

--- main.py
+++ main.py
@@ -11,5 +11,45 @@ def show_inventory():
     return list_info
 
 
+def rent_item():
+    """ str, int -> str
+    Display the items available to rent from. Allows the customer to select the
+    item of their choice and the quantity of that item.
+    """
+    rent = [100, 175, 225]
+    tax = 0.07  # (7%)
+
+# def purchase_item():
+#     """ str, int -> float
+#     Display the items available to purchase from. Allows the customer to select
+#     the item of their choice and the quantity of that item.
+#     """
+#
+#
+# def return_item():
+#     """ str, int -> None
+#     Allows the customer to return the item(s) they rented.
+#     """
+#
+#
+# def replace_item():
+#     """ str, int -> None
+#     User inputs whether it's damaged or not. After, the item is replaced, it
+#     will calculate the total amount owed and prints it out to the customer.
+#     """
+#
+#
+# def test_total_sales_cost():
+#     """ str -> float
+#     Returns the price (including the tax, 7%) calculated of the items selected.
+#     """
+#
+#
+# def update_inventory():
+#     """ str -> None
+#     Update the file every time a new transaction is processed and will write it to a .csv file.
+#     """
+
 if __name__ == '__main__':
+    who = input('Are you administration or customer?').lower().strip()
     print(show_inventory())
--- test_functions.py
+++ test_functions.py
@@ -10,19 +10,19 @@ def test_show_inventory():
                                     'Jurassic Adventure Course (2)',
                                     'YRP Bounce Slide and Pool Combo (5)']]
 
-# def test_rent():
-#     assert main.rent('Disney Princess Bouncer', 1) == 107.0
-#     assert main.rent('Jurassic Adventure Course', 1) == 240.75
+# def test_rent_item():
+#     assert main.rent_item('Disney Princess Bouncer', 1) == 107.0
+#     assert main.rent_item('Jurassic Adventure Course', 1) == 240.75
 #
 #
-# def test_purchase():
-#     assert main.purchase('YRP Bounce Slide and Pool Combo', 1) == 668.75
-#     assert main.purchase('Dolphin Slide', 2) == 1177.0
+# def test_purchase_item():
+#     assert main.purchase_item('YRP Bounce Slide and Pool Combo', 1) == 668.75
+#     assert main.purchase_item('Dolphin Slide', 2) == 1177.0
 #
 #
-# def test_return():
-#     assert main.returned('Sports Bouncer', 1) == 10.7
-#     assert main.returned('16’ Wave Pool Slide', 3) == 56.2
+# def test_return_item():
+#     assert main.return_item('Sports Bouncer', 1) == 10.7
+#     assert main.return_item('16’ Wave Pool Slide', 3) == 56.2
 #
 #
 # def test_total_sales_cost(rented_item):
